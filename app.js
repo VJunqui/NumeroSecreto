2a02c17635ab25995f5dfa311b8a561f8de402a7
Marcel Vitor Junqui <163474051+VJunqui@users.noreply.github.com>
2025-02-03 02:26:40 -0300

    funções criadas

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// SUBSTITUÍDO POR FUNÇÃO
 
+
+// CRIANDO FUNÇÃO
+let numeroSecreto = gerarNumero();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag)
+campo.innerHTML = texto;
+}
+
+// EXECUTANDO FUNÇÃO
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+ 
 function verificarChute() {
-    console.log('O botçao foi clicado');
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumero() {
+    return parseInt(Math.random() * 10 + 1);
 }
